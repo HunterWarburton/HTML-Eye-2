c982b175dfbc32165b66e4498e88f49ad35468f1
HunterWarburton <warburtonhunter@gmail.com>
2022-12-30 11:36:23 -0700

    Create eyeHTML.js

---
+++ eyeHTML.js
@@ -0,0 +1,16 @@
+const eye = document.getElementById('eye');
+const pupil = document.getElementById('pupil');
+
+eye.addEventListener('mousemove', (event) => {
+  const x = event.pageX - eye.offsetLeft - pupil.offsetWidth / 2;
+  const y = event.pageY - eye.offsetTop - pupil.offsetHeight / 2;
+  
+  const maxX = eye.offsetWidth - pupil.offsetWidth;
+  const maxY = eye.offsetHeight - pupil.offsetHeight;
+  
+  x = Math.min(Math.max(x, 0), maxX);
+  y = Math.min(Math.max(y, 0), maxY);
+  
+  pupil.style.left = `${x}px`;
+  pupil.style.top = `${y}px`;
+});
